8ca4d84199aff5b678bda5e7432bf5cc7d1199ad
ito takumi <ito.t.bv@m.titech.ac.jp>
2019-06-18 16:20:34 +0900

    a

--- src/array.c
+++ src/array.c
@@ -8,11 +8,17 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    lb = -1;
+    ub = n;
+  while(ub - lb > 1){
+      int m = (ub+lb)/2;
+      if (A[m] >= k) ub = m;
+      else lb = m;
   }
 
-
-
+    printf("%d¥n", ub);
   return 0;
 }
