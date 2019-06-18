4363cf71289fa954d0703198dcad728793e7a4ea
ito takumi <ito.t.bv@m.titech.ac.jp>
2019-06-18 17:02:28 +0900

    a

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,28 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int x){
+    int i,y;
+    y = 0;
+    for (i = 0; i < n; i++){
+        y = y + (A[i]+x-1)/x;
+    }
+    return y <= k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = A[n-1];
+    while(ub-lb>1){
+        int m = (ub+lb)/2;
+        if(p(m)) {ub = m;}
+        else {lb = m;}
+    }
+    
+    printf("%d¥n", ub);
   return 0;
 }
